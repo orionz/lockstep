f782c3d01eadde6c2142c13a3a6b88cda384bae2
Orion Henry <orion@heroku.com>
2011-06-10 17:04:20 +0100

    refactor the analyze/digest process to be more clear

--- src/lockstep.erl
+++ src/lockstep.erl
@@ -7,31 +7,27 @@
 
 -export([start/2, start/3, start_link/2, start_link/3, ets/1 ]).
 
--record(state, { socket, ets, dets, host, port, path, digest, transfer, contentlength, writes=0 }).
+-record(state, { socket, ets, dets, host, port, path, schema, transfer, contentlength, writes=0 }).
 
-start(Url, Digest) -> start(Url, Digest, null).
+start(Url, Schema) -> start(Url, Schema, null).
 
-start(Url, Digest, Dets) -> gen_server:start(?MODULE, [Url, Digest, Dets], []).
+start(Url, Schema, Dets) -> gen_server:start(?MODULE, [Url, Schema, Dets], []).
 
-start_link(Url, Digest) -> start_link(Url, Digest, null).
+start_link(Url, Schema) -> start_link(Url, Schema, null).
 
-start_link(Url, Digest, Dets) -> gen_server:start_link(?MODULE, [Url, Digest, Dets], []).
+start_link(Url, Schema, Dets) -> gen_server:start_link(?MODULE, [Url, Schema, Dets], []).
 
 ets(Pid) -> gen_server:call(Pid,ets).
 
 %% handle URL path that does not end in '/'
 %% handle redirects
 
-init([Uri, Digest, DetsFile]) when is_list(Uri) and is_tuple(Digest) ->
+init([Uri, Schema, DetsFile]) when is_list(Uri) and is_tuple(Schema) ->
   {http,[],Host, Port, Path, []} = http_uri:parse(Uri),
   { Ets, Dets } = setup_tables(DetsFile),
   erlang:send(self(), connect),
   erlang:send_after(10000, self(), stats),
-  Digest2 = tuple_to_list(Digest),
-  Digest3 = lists:map(fun erlang:atom_to_list/1, Digest2),
-  Digest4 = lists:map(fun erlang:list_to_binary/1, Digest3),
-  io:format("Digest ~p~n",[Digest4]),
-  { ok, #state{ets=Ets, dets=Dets, host=Host, port=Port, path=Path, digest=Digest4 } }.
+  { ok, #state{ets=Ets, dets=Dets, host=Host, port=Port, path=Path, schema=digest_schema(Schema) } }.
 
 handle_call(ets, _From, State) ->
     {reply, State#state.ets, State};
@@ -109,28 +105,25 @@ read_chunk(Socket, Bytes, State) ->
 
 process_chunk(Chunk, State) ->
   case mochijson2:decode(Chunk) of
-    { struct, Props } -> analyze(Props, State)
+    { struct, Props } -> process_proplist(Props, State)
   end,
   State.
 
-analyze(Props, #state{digest=Digest}=State) ->
-  io:format("PROPS: ~p~n",[Props]),
-  Target = erlang:make_tuple( length(Digest), null),
-  { ok, Action, Time, Record } = X = analyze(Props, set, 0, Digest, Target),
-  io:format("Digested Action: ~p~n",[X]),
-  case Action of
-    set -> set(Time, Record, State);
-    delete -> delete(Time, Record, State)
-  end.
+process_proplist(Props, #state{schema=Schema}=State) ->
+  { ok, Meta, Record } = analyze(Props, Schema),
+  perform_update(Meta, Record, State).
 
-analyze([{<<"deleted_at">>, DeletedAt }=P | Props], _Action, UpdatedAt, Digest, Target) when is_integer(DeletedAt) ->
-  analyze(Props, delete, UpdatedAt, Digest, digest(P, Digest, Target, 1));
-analyze([{<<"updated_at">>, UpdatedAt}=P | Props], Action, _UpdatedAt, Digest, Target) when is_integer(UpdatedAt) ->
-  analyze(Props, Action, UpdatedAt, Digest, digest(P, Digest, Target, 1));
-analyze([ P | Props], Action, UpdatedAt, Digest, Target) ->
-  analyze(Props, Action, UpdatedAt, Digest, digest(P, Digest, Target, 1));
-analyze([], Action, UpdatedAt, _Digest, Target) ->
-  { ok, Action, UpdatedAt, Target }.
+analyze(Props, Schema) ->
+  BlankRecord = erlang:make_tuple( length(Schema), null),
+  BlankMeta = { set, 0 },
+  analyze(Props, Schema, BlankRecord, BlankMeta).
+
+analyze([ P | Props], Schema, Record, Meta) ->
+  NewMeta = build_meta(P, Meta),
+  NewRecord = build_record(P, Schema, Record, 1),
+  analyze(Props, Schema, NewRecord, NewMeta);
+analyze([], _, Record, Meta) ->
+  { ok, Meta, Record }.
 
 head(State) ->
   case ets:lookup(State#state.ets, lockstep_head) of
@@ -138,12 +131,24 @@ head(State) ->
     _ -> 0
   end.
 
-digest({ Key, Value}, [ Key | _ ], Tuple, Index ) ->
+build_meta({<<"deleted_at">>, DeletedAt}, { _, UpdatedAt }) when is_integer(DeletedAt) ->
+  { delete, UpdatedAt };
+build_meta({<<"updated_at">>, UpdatedAt}, { Action, _ }) ->
+  { Action, UpdatedAt};
+build_meta(_, Meta) ->
+  Meta.
+
+build_record({ Key, Value}, [ Key | _ ], Tuple, Index ) ->
   erlang: setelement( Index, Tuple, Value );
-digest({ Key, Value}, [ _ | Tail ], Tuple, Index ) -> 
-  digest({ Key, Value}, Tail, Tuple, Index + 1 );
-digest(_, [], Tuple, _ ) -> 
+build_record({ Key, Value}, [ _ | Tail ], Tuple, Index ) ->
+  build_record({ Key, Value}, Tail, Tuple, Index + 1 );
+build_record(_, [], Tuple, _ ) ->
   Tuple.
+
+perform_update( { set, Time }, Record, State ) ->
+  set(Time, Record, State);
+perform_update( { delete, Time }, Record, State ) ->
+  delete(Time, Record, State).
 
 set(Time, Record, State) ->
   io:format("SET ~p ~p~n",[Time, Record]),
@@ -180,6 +185,11 @@ disconnect(State) ->
   gen_tcp:close(State#state.socket),
   erlang:send_after(3000, self(), connect),
   State#state{socket=null}.
+
+digest_schema(Schema) ->
+  Schema2 = tuple_to_list(Schema),
+  Schema3 = lists:map(fun erlang:atom_to_list/1, Schema2),
+  lists:map(fun erlang:list_to_binary/1, Schema3).
 
 connect(State) ->
   Opts = [binary, {packet, http_bin}, {packet_size, 1024 * 1024}, {recbuf, 1024 * 1024}, {active, once}],
